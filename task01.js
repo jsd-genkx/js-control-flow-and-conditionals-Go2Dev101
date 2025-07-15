10a9297b383070b9a6d15619d13964de2d12bda0
supasin <dev.snakphu7@gmail.com>
2025-07-15 11:41:35 +0700

    task01 solution

--- task01.js
+++ task01.js
@@ -3,3 +3,12 @@
 // TODO: Use if/else to print "Even" or "Odd"
 
 const number = 15;
+    console.log(number)
+
+if (number % 2 === 0) {
+    // Code to execute if condition is true'
+    console.log("Even")
+} else {
+    console.log("Odd")
+    // Code to execute if all conditions are false
+}
