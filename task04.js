7d146f4baaf7dfb4c2a22106c6b4a6d3d4cb3d81
supasin <dev.snakphu7@gmail.com>
2025-07-15 14:42:21 +0700

    task04 solution

--- task04.js
+++ task04.js
@@ -9,3 +9,37 @@
 // TODO BONUS: Rewrite with if
 
 const num = 15;
+
+if (num % 3 === 0 && num % 5 === 0) {
+    console.log("FizzBuzz");
+
+}
+    // Code to execute if condition is true
+else if (num % 3 === 0) {
+    // Code to execute if anotherCondition is true
+    console.log("Fizz");
+
+}
+else if (num % 5 === 0) {
+    // Code to execute if anotherCondition is true
+    console.log("Buzz");
+
+}else {
+    // Code to execute if all conditions are false
+    console.log("Not divisible by 3 or 5");
+
+}
+
+// New Step Learning บวกทีละบรรทัดสำหรับ +=
+
+let result = "";
+if (num % 3 === 0) {
+  result += "Fizz";
+}
+if (num % 5 === 0) {
+  result += "Buzz";
+}
+if (result === "") {
+  result = "Not divisible by 3 or 5";
+}
+console.log(result);
